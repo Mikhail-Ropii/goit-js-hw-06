5064d631b30bd05c927c8d9cfd3c3268a9c6f479
Mikhail-Ropii <mr.ropiy@gmail.com>
2021-12-06 21:22:49 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorValue = document.querySelector('.color')
+
+btnEl.addEventListener('click', changeColor);
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+}
